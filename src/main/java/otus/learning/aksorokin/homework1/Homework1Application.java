c14123b464463ca91d08319648c6ba0ca2d1f5b0
aksorokin <aksorokin@gmail.com>
2021-09-01 23:35:16 +0300

    init

--- src/main/java/otus/learning/aksorokin/homework1/Homework1Application.java
+++ src/main/java/otus/learning/aksorokin/homework1/Homework1Application.java
@@ -18,10 +18,32 @@ public class Homework1Application implements CommandLineRunner {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
+
     @Override
     public void run(String... args) throws Exception {
         logger.debug("Creating database tables");
+        jdbcTemplate.execute("CREATE SEQUENCE education.users_seq");
         jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS education.users (\n" +
+                "                  id bigint check (id > 0) NOT NULL DEFAULT NEXTVAL ('education.users_seq'),\n" +
+                "                  username varchar(100) NOT NULL,\n" +
+                "                  password varchar(100) NOT NULL,\n" +
+                "                  enabled smallint NOT NULL,\n" +
+                "                  lastname varchar(100) NOT NULL,\n" +
+                "                  age int NOT NULL,\n" +
+                "                  interests varchar(1000) DEFAULT NULL,\n" +
+                "                  city varchar(50) NOT NULL,\n" +
+                "                  gender smallint NOT NULL,\n" +
+                "                  PRIMARY KEY (id),\n" +
+                "                  CONSTRAINT username_unique UNIQUE (username)\n" +
+                "                )   \n" +
+                "\n" +
+                "alter sequence education.users_seq restart with 7;");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS education.user_friends (\n" +
+                "                   user_id bigint NOT NULL,\n" +
+                "                   friend_id bigint NOT NULL,\n" +
+                "                   PRIMARY KEY (friend_id,user_id)\n" +
+                "                )");
+  /*      jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS education.users (\n" +
                 "  id bigint unsigned NOT NULL AUTO_INCREMENT,\n" +
                 "  username varchar(100) NOT NULL,\n" +
                 "  password varchar(100) NOT NULL,\n" +
@@ -41,5 +63,7 @@ public class Homework1Application implements CommandLineRunner {
                 "   PRIMARY KEY (friend_id,user_id)\n" +
                 ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci" +
                 "");
+
+    */
     }
 }
